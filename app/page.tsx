2e43621076f0d26f17eabb57bb30ab96b9370582
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-07 12:39:57 -0600

    feat: add pricing, contact, faq and footer to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,22 @@ import Benefits from "./components/Benefits";
 import HowWeWork from "./components/HowWeWork";
 import Items from "./components/Items";
 import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <Hero />
-      <Benefits />
       <HowWeWork />
       <Items />
       <Services />
+      <Pricing />
+      <Contact />
+      <FAQ />
+      <Footer />
     </main>
   );
 }
