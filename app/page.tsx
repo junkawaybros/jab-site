e9c9ff1540366aa11a8a3b5fa66a99e54e855827
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-06 19:03:43 -0600

    feat: create Benefits component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Benefits />
     </main>
   );
 }
