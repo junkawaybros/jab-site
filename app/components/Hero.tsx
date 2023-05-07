fd75c52415cff61e4a7a35283a96f5b5410d3594
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-07 12:40:19 -0600

    feat: add benefits component to hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import HeroJunk from "../../public/hero-junk.jpg";
+import Benefits from "./Benefits";
 
 export default function Hero() {
   return (
-    <section className="bg-gray-900">
+    <section className="bg-white">
       <div className="relative isolate overflow-hidden">
         <div className="mx-auto max-w-7xl pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-20">
           <div className="px-6 lg:px-0 lg:pt-4">
             <div className="mx-auto max-w-2xl">
               <div className="max-w-lg">
-                <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-50 sm:text-4xl">
+                <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-950 sm:text-4xl">
                   Breathe Easy: Your Path to a Clutter-Free Environment Starts
                   Here
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-300">
+                <p className="mt-6 text-lg leading-8 text-gray-900">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                   qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
                   occaecat fugiat aliqua.
@@ -21,13 +22,13 @@ export default function Hero() {
                 <div className="items-left mt-10 flex flex-col items-baseline gap-x-6 gap-y-6 sm:flex-row">
                   <a
                     href="#"
-                    className="text-md rounded-md bg-teal-500 px-3.5 py-2.5 font-semibold uppercase text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500 sm:text-lg"
+                    className="text-md sm:text-md rounded-md bg-teal-500 px-3.5 py-2.5 font-semibold uppercase text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500"
                   >
                     Get a free estimate
                   </a>
                   <a
                     href="#"
-                    className="text-md flex font-semibold uppercase leading-6 text-white hover:text-teal-400"
+                    className="text-md flex font-semibold uppercase leading-6 text-teal-500 hover:text-teal-400"
                   >
                     <span className="mr-2">Learn more</span>
                     <svg
@@ -49,7 +50,7 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div className="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
+          <div className="mt-20 sm:mt-24 md:mx-auto md:max-w-xl lg:mx-0 lg:mt-0 lg:w-screen">
             {/* <!-- <div className="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36" aria-hidden="true"></div> --> */}
             <Image
               className="md:rounded-lg"
@@ -60,6 +61,7 @@ export default function Hero() {
             />
           </div>
         </div>
+        <Benefits />
       </div>
     </section>
   );
