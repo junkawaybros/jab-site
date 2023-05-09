92057db273cfe2397bd2acfe75310605889bb2da
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-09 08:33:51 -0600

    remove nav from footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,4 @@
 const navigation = {
-  main: [
-    { name: "About", href: "#" },
-    { name: "Blog", href: "#" },
-    { name: "Jobs", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Accessibility", href: "#" },
-    { name: "Partners", href: "#" },
-  ],
   social: [
     {
       name: "Facebook",
@@ -75,22 +67,7 @@ const navigation = {
 export default function Footer() {
   return (
     <footer className="bg-gray-900" id="footer">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav
-          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-          aria-label="Footer"
-        >
-          {navigation.main.map((item) => (
-            <div key={item.name} className="pb-6">
-              <a
-                href={item.href}
-                className="text-sm leading-6 text-white hover:text-teal-400"
-              >
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </nav>
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 pb-20 lg:px-8">
         <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
             <a
