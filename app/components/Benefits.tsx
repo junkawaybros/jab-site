5b8f4f210f9cd8617efd8a0091566affe588bc7b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-17 16:42:31 -0600

    feat: add content

--- app/components/Benefits.tsx
+++ app/components/Benefits.tsx
@@ -1,4 +1,38 @@
-import React from "react";
+import Image from "next/image";
+
+const data = [
+  {
+    id: 1,
+    img: "/delivery-truck.svg",
+    alt: "Delivery Truck",
+    title: "Quick and Easy",
+    description: "We'll remove your junk quickly so you can focus on your day.",
+  },
+  {
+    id: 2,
+    img: "/budget.svg",
+    alt: "Budget",
+    title: "Transparent Pricing",
+    description:
+      "Our pricing is straightforward with no hidden fees or surprises. You'll always know the final price before we start.",
+  },
+  {
+    id: 3,
+    img: "/calendar.svg",
+    alt: "Calendar",
+    title: "Same Day Junk Removal",
+    description:
+      "Need it done now? No problem! We offer same-day services to help you get rid of your junk as soon as possible.",
+  },
+  {
+    id: 4,
+    img: "/broom.svg",
+    alt: "Broom",
+    title: "Complimentary Clean-up",
+    description:
+      "Every job comes with a free clean-up so you don't have to worry about the mess after we're done.",
+  },
+];
 
 export default function Benefits() {
   return (
@@ -9,86 +43,30 @@ export default function Benefits() {
 
       <div className="mx-auto max-w-7xl px-4 py-20 sm:px-6 sm:pb-32 lg:px-8">
         <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-0">
-          <div className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
-            <div className="md:flex-shrink-0">
-              <div className="flow-root">
-                <img
-                  className="-my-1 mx-auto h-24 w-auto"
-                  src="https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg"
-                  alt=""
-                />
+          {data.map((item) => (
+            <div
+              key={item.id}
+              className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+            >
+              <div className="md:flex-shrink-0">
+                <div className="flow-root">
+                  <Image
+                    className="-my-1 mx-auto h-14 w-auto"
+                    src={item.img}
+                    alt={item.alt}
+                    width={96}
+                    height={96}
+                  />
+                </div>
+              </div>
+              <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                <h3 className="text-base font-medium text-gray-950">
+                  {item.title}
+                </h3>
+                <p className="mt-3 text-sm text-gray-900">{item.description}</p>
               </div>
             </div>
-            <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-              <h3 className="text-base font-medium text-gray-950">
-                Free returns
-              </h3>
-              <p className="mt-3 text-sm text-gray-900">
-                Not what you expected? Place it back in the parcel and attach
-                the pre-paid postage stamp.
-              </p>
-            </div>
-          </div>
-          <div className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
-            <div className="md:flex-shrink-0">
-              <div className="flow-root">
-                <img
-                  className="-my-1 mx-auto h-24 w-auto"
-                  src="https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-              <h3 className="text-base font-medium text-gray-950">
-                Same day/next day
-              </h3>
-              <p className="mt-3 text-sm text-gray-900">
-                We offer a delivery service that has never been done before.
-                Checkout today and receive your products within hours.
-              </p>
-            </div>
-          </div>
-          <div className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
-            <div className="md:flex-shrink-0">
-              <div className="flow-root">
-                <img
-                  className="-my-1 mx-auto h-24 w-auto"
-                  src="https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-              <h3 className="text-base font-medium text-gray-950">
-                All year discount
-              </h3>
-              <p className="mt-3 text-sm text-gray-900">
-                Looking for a deal? You can use the code &quot;ALLYEAR&quot; at
-                checkout and get money off all year round.
-              </p>
-            </div>
-          </div>
-          <div className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
-            <div className="md:flex-shrink-0">
-              <div className="flow-root">
-                <img
-                  className="-my-1 mx-auto h-24 w-auto"
-                  src="https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-              <h3 className="text-base font-medium text-gray-950">
-                For the planet
-              </h3>
-              <p className="mt-3 text-sm text-gray-900">
-                We’ve pledged 1% of sales to the preservation and restoration of
-                the natural environment.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
