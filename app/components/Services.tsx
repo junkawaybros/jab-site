149e32b93dab6076f2559339ead09fb9c9c44e6a
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2023-05-17 16:43:34 -0600

    feat: add content

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,221 +1,132 @@
 import React from "react";
 
+const data = {
+  title: " Professional High-Volume Junk and Clutter Cleanouts",
+  subtitle: "Big Mess? No Stress. Big Cleanups Made Easy",
+  description: `Junk removal pros for any job, big or small! We clean out homes, offices, 
+  and storage units with speed and efficiency. Our team handles everything from single-item pickups 
+  to large cleanouts, even the toughest jobs like hoarding or construction debris. Trust The Bros for a clutter-free space!`,
+  items: [
+    {
+      id: 1,
+      name: "Attic & Crawlspace Cleanouts",
+      description:
+        "Attics and crawlspaces often become the default storage areas in a home. From old furniture and clothing to unused appliances and holiday decorations, we help you clear out these often-neglected spaces, enhancing your storage capacity and improving your home's safety.",
+    },
+    {
+      id: 2,
+      name: "Basement Cleanouts",
+      description:
+        "Basements can quickly become cluttered with unused items and old junk. Our team will help you sort, remove, and dispose of these items responsibly, transforming your basement into a usable and organized space.",
+    },
+    {
+      id: 3,
+      name: "Garage Cleanouts",
+      description:
+        "Over time, garages can become overrun with tools, sports equipment, old furniture, and more. We assist you in clearing out unwanted items, freeing up space for your vehicle or creating a more functional area for your hobbies or storage.",
+    },
+    {
+      id: 4,
+      name: "Home Cleanouts",
+      description:
+        "Get your home cleaned out quickly with our all-inclusive services for moving, downsizing, or decluttering. We'll remove old furniture, appliances, clothing, and more from every room in the house.",
+    },
+    {
+      id: 5,
+      name: "Apartment Cleanouts",
+      description:
+        "We offer cleanout services for renters/apartment managers: old furniture, unwanted appliances removed for fresh start/new tenants. Every room is cleared and ready for its next chapter.",
+    },
+    {
+      id: 6,
+      name: "Storage Unit Cleanouts",
+      description:
+        "Clear out your storage unit with ease! Our storage unit clear-out services make it easy to get rid of unwanted items and organize the space. We provide fast, efficient service that will have your storage unit looking clean and organized in no time.",
+    },
+    {
+      id: 7,
+      name: "Estate Cleanouts",
+      description:
+        "We manage estate cleanouts by sorting furniture, electronics, & possessions after a loved one passes or moves to care. Proper disposal & donation are our priority.",
+    },
+    {
+      id: 8,
+      name: "Bereavement Cleanouts",
+      description:
+        "We'll handle bereavement cleanouts with empathy and patience. Our pros ensure the process is smooth and stress-free so you have space to grieve.",
+    },
+    {
+      id: 9,
+      name: "Commercial/Office Cleanouts",
+      description:
+        "Revamping, relocating or closing? We've got you covered. Our cleanout service takes care of the office mess, obsolete tech and surplus inventory for a spotless commercial space ready for a new lease on life.",
+    },
+    {
+      id: 10,
+      name: "Office furniture Removal",
+      description:
+        "Clear your workspace with ease! Our Furniture Removal service dismantles, moves and disposes of your old furniture while causing minimal disruption to your business. We'll recycle or donate your items responsibly. Book us after hours or on weekends for an organized, efficient office.",
+    },
+    {
+      id: 11,
+      name: "Foreclosure Cleanouts",
+      description:
+        "For those in need of a prompt, efficient approach to address the consequences of foreclosure, our team provides cleanouts and recycling services for real estate agents, banks, and property owners. Make it easy on yourself to ready the property for sale or rent by taking advantage of our Foreclosure Cleanouts services and save time in the process!",
+    },
+    {
+      id: 12,
+      name: "Hoarding Cleanouts",
+      description:
+        "We offer non-judgmental assistance in carefully planning, sorting, and disposing of excess belongings. We also donate/recycle usable items & dispose of waste responsibly. We help improve lives by reclaiming living spaces.",
+    },
+    {
+      id: 13,
+      name: "Construction Debris & Waste Removal",
+      description:
+        "Our construction waste removal service handles disposal during building, renovation, or demolition projects. We recycle wood, drywall, metal, concrete, glass, and insulation. We ensure efficiency, safety, and eco-friendliness to keep projects on schedule and clutter-free.",
+    },
+  ],
+};
+
 export default function Services() {
   return (
     <section className="bg-gray-900 py-20 sm:py-32" id="services">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-teal-400">
-            High-Volume Junk and Clutter Cleanouts
+            {data.title}
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Big Cleanups Made Easy: Professional Large Scale Junk Removal
-            Services
+            {data.subtitle}
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
-            In mi viverra elit nunc.
+            {data.description}
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Attic & Crawlspace Cleanout
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Commodo nec sagittis tortor mauris sed. Turpis tortor quis
-                  scelerisque diam id accumsan nullam tempus. Pulvinar etiam
-                  lacus volutpat eu. Phasellus praesent ligula sit faucibus.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
+            {data.items.map((item) => (
+              <div className="flex flex-col" key={item.id}>
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                  <svg
+                    className="absolute left-0 top-1 h-5 w-5 text-teal-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
                   >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Attic & Crawlspace Cleanout
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Commodo nec sagittis tortor mauris sed. Turpis tortor quis
-                  scelerisque diam id accumsan nullam tempus. Pulvinar etiam
-                  lacus volutpat eu. Phasellus praesent ligula sit faucibus.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Attic & Crawlspace Cleanout
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Commodo nec sagittis tortor mauris sed. Turpis tortor quis
-                  scelerisque diam id accumsan nullam tempus. Pulvinar etiam
-                  lacus volutpat eu. Phasellus praesent ligula sit faucibus.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Attic & Crawlspace Cleanout
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Commodo nec sagittis tortor mauris sed. Turpis tortor quis
-                  scelerisque diam id accumsan nullam tempus. Pulvinar etiam
-                  lacus volutpat eu. Phasellus praesent ligula sit faucibus.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Basement Cleanout Services
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Pellentesque enim a commodo malesuada turpis eleifend risus.
-                  Facilisis donec placerat sapien consequat tempor fermentum
-                  nibh.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-            <div className="flex flex-col">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                <svg
-                  className="h-5 w-5 flex-none text-teal-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Estate Cleanout
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                <p className="flex-auto">
-                  Pellentesque sit elit congue ante nec amet. Dolor aenean
-                  curabitur viverra suspendisse iaculis eget. Nec mollis
-                  placerat ultricies euismod ut condimentum.
-                </p>
-                <p className="mt-6">
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-teal-400"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  {item.name}
+                </dt>
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                  <p className="flex-auto">{item.description}</p>
+                </dd>
+              </div>
+            ))}
           </dl>
         </div>
       </div>
